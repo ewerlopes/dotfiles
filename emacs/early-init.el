137a1eb559486a5cba2739d6d88b62e4bff1c4fd
Ewerton de Oliveira <ewerlopes@gmail.com>
2025-11-11 22:35:43 -0300

    style: adjust indentation for better readability in early-init.el

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,12 +1,12 @@
-(setq package-enable-at-startup nil
-      inhibit-startup-message   t
-      visual-line-fringe-indicators nil ; remove fringe bitmaps on wrapped lines
-      frame-resize-pixelwise    t  ; fine resize
-      package-native-compile    t) ; native compile packages
-(scroll-bar-mode -1)               ; disable scrollbar
-(tool-bar-mode -1)                 ; disable toolbar
-(tooltip-mode -1)                  ; disable tooltips
-(set-fringe-mode 10)               ; give some breathing room
-(menu-bar-mode -1)                 ; disable menubar
-(global-display-line-numbers-mode) ; enable lines globally
-(global-visual-line-mode 1)        ; enable line wrap
+  (setq package-enable-at-startup nil
+        inhibit-startup-message   t
+        visual-line-fringe-indicators nil ; remove fringe bitmaps on wrapped lines
+        frame-resize-pixelwise    t  ; fine resize
+        package-native-compile    t) ; native compile packages
+  (scroll-bar-mode -1)               ; disable scrollbar
+  (tool-bar-mode -1)                 ; disable toolbar
+  (tooltip-mode -1)                  ; disable tooltips
+  (set-fringe-mode 10)               ; give some breathing room
+  (menu-bar-mode -1)                 ; disable menubar
+  (global-display-line-numbers-mode) ; enable lines globally
+  (global-visual-line-mode 1)        ; enable line wrap
