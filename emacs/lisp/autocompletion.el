b73bbb2d7aa693569ac06cb3cc6d31d9e872aa48
Ewerton de Oliveira <ewerlopes@gmail.com>
2026-01-04 20:22:10 -0300

    feat: use Vertico + Consult + Corfu and others instead of  Ivy/Counsel/Swiper/Company.

--- emacs/lisp/autocompletion.el
+++ emacs/lisp/autocompletion.el
@@ -1,159 +1,62 @@
-(use-package company
+(use-package vertico
   :straight (:build t)
-  :defer t
-  :hook (company-mode . evil-normalize-keymaps)
-  :init (global-company-mode)
-  :config
-  (setq company-minimum-prefix-length     2
-        company-toolsip-limit             14
-        company-tooltip-align-annotations t
-        company-require-match             'never
-        company-global-modes              '(not erc-mode message-mode help-mode gud-mode)
-        company-frontends
-        '(company-pseudo-tooltip-frontend ; always show candidates in overlay tooltip
-          company-echo-metadata-frontend) ; show selected candidate docs in echo area
-        company-backends '(company-capf)
-        company-auto-commit         nil
-        company-auto-complete-chars nil
-        company-dabbrev-other-buffers nil
-        company-dabbrev-ignore-case nil
-        company-dabbrev-downcase    nil))
-
-(use-package company-dict
-  :after company
-  :straight (:build t)
-  :config
-  (setq company-dict-dir (expand-file-name "dicts" user-emacs-directory)))
-
-(use-package company-box
-  :straight (:build t)
-  :after (company all-the-icons)
-  :config
-  (setq company-box-show-single-candidate t
-        company-box-backends-colors       nil
-        company-box-max-candidates        50
-        company-box-icons-alist           'company-box-icons-all-the-icons
-        company-box-icons-all-the-icons
-        (let ((all-the-icons-scale-factor 0.8))
-          `(
-            (Unknown . ,(all-the-icons-material "find_in_page" :face 'all-the-icons-purple))
-            (Text . ,(all-the-icons-material "text_fields" :face 'all-the-icons-green))
-            (Method . ,(all-the-icons-material "functions" :face 'all-the-icons-red))
-            (Function . ,(all-the-icons-material "functions" :face 'all-the-icons-red))
-            (Constructor . ,(all-the-icons-material "functions" :face 'all-the-icons-red))
-            (Field . ,(all-the-icons-material "functions" :face 'all-the-icons-red))
-            (Variable . ,(all-the-icons-material "adjust" :face 'all-the-icons-blue))
-            (Class . ,(all-the-icons-material "class" :face 'all-the-icons-red))
-            (Interface . ,(all-the-icons-material "settings_input_component" :face 'all-the-icons-red))
-            (Module . ,(all-the-icons-material "view_module" :face 'all-the-icons-red))
-            (Property . ,(all-the-icons-material "settings" :face 'all-the-icons-red))
-            (Unit . ,(all-the-icons-material "straighten" :face 'all-the-icons-red))
-            (Value . ,(all-the-icons-material "filter_1" :face 'all-the-icons-red))
-            (Enum . ,(all-the-icons-material "plus_one" :face 'all-the-icons-red))
-            (Keyword . ,(all-the-icons-material "filter_center_focus" :face 'all-the-icons-red))
-            (Snippet . ,(all-the-icons-material "short_text" :face 'all-the-icons-red))
-            (Color . ,(all-the-icons-material "color_lens" :face 'all-the-icons-red))
-            (File . ,(all-the-icons-material "insert_drive_file" :face 'all-the-icons-red))
-            (Reference . ,(all-the-icons-material "collections_bookmark" :face 'all-the-icons-red))
-            (Folder . ,(all-the-icons-material "folder" :face 'all-the-icons-red))
-            (EnumMember . ,(all-the-icons-material "people" :face 'all-the-icons-red))
-            (Constant . ,(all-the-icons-material "pause_circle_filled" :face 'all-the-icons-red))
-            (Struct . ,(all-the-icons-material "streetview" :face 'all-the-icons-red))
-            (Event . ,(all-the-icons-material "event" :face 'all-the-icons-red))
-            (Operator . ,(all-the-icons-material "control_point" :face 'all-the-icons-red))
-            (TypeParameter . ,(all-the-icons-material "class" :face 'all-the-icons-red))
-            (Template . ,(all-the-icons-material "short_text" :face 'all-the-icons-green))
-            (ElispFunction . ,(all-the-icons-material "functions" :face 'all-the-icons-red))
-            (ElispVariable . ,(all-the-icons-material "check_circle" :face 'all-the-icons-blue))
-            (ElispFeature . ,(all-the-icons-material "stars" :face 'all-the-icons-orange))
-            (ElispFace . ,(all-the-icons-material "format_paint" :face 'all-the-icons-pink))
-            ))))
-
-(use-package counsel
-  :straight (:build t)
-  :ensure t
-  :after recentf
-  :after ivy
-  :bind (("M-x"     . counsel-M-x)
-         ("C-x b"   . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history)))
-
-(use-package ivy
-  :straight t
-  :defer t
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-t" . ivy-next-line)
-         ("C-s" . ivy-previous-line)
-         ("C-u" . ivy-scroll-up-command)
-         ("C-d" . ivy-scroll-down-command)
-         :map ivy-switch-buffer-map
-         ("C-t" . ivy-next-line)
-         ("C-s" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-t" . ivy-next-line)
-         ("C-s" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1)
-  (setq ivy-wrap                        t
-        ivy-height                      17
-        ivy-sort-max-size               50000
-        ivy-fixed-height-minibuffer     t
-        ivy-read-action-functions       #'ivy-hydra-read-action
-        ivy-read-action-format-function #'ivy-read-action-format-columns
-        projectile-completion-system    'ivy
-        ivy-on-del-error-function       #'ignore
-        ivy-use-selectable-prompt       t))
-
-(use-package ivy-prescient
-  :after ivy
-  :straight (:build t))
-
-(use-package ivy-hydra
-  :requires (ivy hydra)
-  :after ivy
-  :straight (:build t))
-
-(use-package ivy-rich
-  :straight (:build t)
-  :after (ivy counsel)
-  :config
-  ;; enable ivy-rich and show docstrings for counsel-M-x candidates
-  (ivy-rich-mode 1)
-  (setq ivy-rich-display-transformers-list
-        (plist-put ivy-rich-display-transformers-list
-                   'counsel-M-x
-                   '(:columns
-                     ((ivy-rich-candidate :width 0.6)
-                      (ivy-rich-function-docstring :face font-lock-doc-face))))))
-
-(use-package yasnippet
-  :defer t
-  :straight (:build t)
+  :custom
+  (vertico-count 20)  ;; limit to a fixed size
+  :bind (:map vertico-map
+    ;; Use page-up/down to scroll vertico buffer, like ivy does by default.
+    ("<prior>" . 'vertico-scroll-down)
+    ("<next>"  . 'vertico-scroll-up))
   :init
-  (yas-global-mode)
-  :hook ((prog-mode . yas-minor-mode)
-         (text-mode . yas-minor-mode)))
+  ;; Activate vertico
+  (vertico-mode))
 
-(use-package yasnippet-snippets
-  :defer t
-  :after yasnippet
-  :straight (:build t))
+(use-package vertico-directory
+  :after vertico
+  :ensure nil  ;; no need to install, it comes with vertico
+  :bind (:map vertico-map
+    ("DEL" . vertico-directory-delete-char)))
 
-(use-package yatemplate
-  :defer t
-  :after yasnippet
-  :straight (:build t))
+(use-package orderless
+  :straight (:build t)
+  :custom
+  ;; Activate orderless completion
+  (completion-styles '(orderless basic))
+  ;; Enable partial completion for file wildcard support
+  (completion-category-overrides '((file (styles partial-completion)))))
 
-(use-package ivy-yasnippet
-  :defer t
-  :after (ivy yasnippet)
-  :straight (:build t))
+(use-package consult
+  :straight (:build t)
+  :custom
+  ;; Disable preview
+  (consult-preview-key nil)
+  :bind
+  (("C-x b" . 'consult-buffer)    ;; Switch buffer, including recentf and bookmarks
+   ("M-l"   . 'consult-git-grep)  ;; Search inside a project
+   ("M-y"   . 'consult-yank-pop)  ;; Paste by selecting the kill-ring
+   ("M-s"   . 'consult-line)      ;; Search current buffer, like swiper
+   ))
+
+(use-package embark
+  :straight (:build t)
+  :bind
+  (("C-."   . embark-act)         ;; Begin the embark process
+   ("C-;"   . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :config
+  (use-package embark-consult))
+
+(use-package marginalia
+  :straight (:build t)
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init section is always executed.
+  :init
+
+  ;; Marginalia must be activated in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package.
+  (marginalia-mode))
