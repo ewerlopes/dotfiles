53b1a00efd59179f2fe8295f1708978a5be16c2c
Ewerton de Oliveira <ewerlopes@gmail.com>
2026-01-04 14:40:45 -0300

    feat: add beancount mode config.

--- emacs/lisp/helpful.el
+++ emacs/lisp/helpful.el
@@ -15,3 +15,20 @@
    "p"   #'bufler-list-buffer-peek
    "s"   #'bufler-list-buffer-save
    "RET" #'bufler-list-buffer-switch))
+
+(use-package beancount
+  :straight (:build t)
+  :ensure t
+  :mode (("\\.beancount\\'" . beancount-mode))
+  :config
+  ;; Optional: Add configuration settings here.
+  ;; Example: Align numbers to a specific column
+  (setq beancount-number-alignment-column 52)
+
+  ;; Optional: Define custom keybindings, e.g. for inserting the current date
+  ;; (global-set-key (kbd "C-c d") 'beancount-insert-date)
+
+  ;; Ensure beancount executable is in the Emacs PATH for tools like bean-check
+  (setenv "PATH" (concat (getenv "PATH") ":/Users/ewerlopes/coding/contas/.venv/bin"))
+  (add-to-list 'exec-path "/Users/ewerlopes/coding/contas/.venv/bin")
+)
